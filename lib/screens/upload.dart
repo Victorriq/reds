b9e0d8a48df17be78f3cd09edd59b9cfc8cd82d5
Victorriq <botsnumero1@gmail.com>
2023-05-27 02:04:14 +0200

    Ya pide permisos y muestra imagenes

--- lib/screens/upload.dart
+++ lib/screens/upload.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPage extends StatefulWidget {
   const UploadPage({Key? key}) : super(key: key);
@@ -13,7 +17,6 @@ class _UploadPageState extends State<UploadPage> {
     'Item 2',
     'Item 3',
     'Item 4'
-
   ];
   List<IconData> icons = [
     Icons.image,
@@ -21,6 +24,52 @@ class _UploadPageState extends State<UploadPage> {
     Icons.attach_money,
     Icons.poll
   ];
+
+  bool showImage = false;
+  XFile? selectedImage;
+
+  Future<void> requestPermissionsAndOpenGallery() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.camera,
+      Permission.storage,
+    ].request();
+
+    if (statuses[Permission.camera]!.isGranted &&
+        statuses[Permission.storage]!.isGranted) {
+      final ImagePicker _picker = ImagePicker();
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        setState(() {
+          showImage = true;
+          selectedImage = image;
+        });
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Permisos requeridos'),
+          content: const Text(
+              'Debes aceptar los permisos de cámara y galería para acceder a la galería.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Aceptar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void clearSelectedImage() {
+    setState(() {
+      showImage = false;
+      selectedImage = null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,33 +80,65 @@ class _UploadPageState extends State<UploadPage> {
       body: Container(
         color: Colors.black,
         padding: const EdgeInsets.only(top: 16),
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Card(
-              elevation: 4,
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              color: Colors.grey[800],
-              child: ListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                leading: Icon(
-                  icons[index],
-                  color: Colors.white,
-                ),
-                title: Text(
-                  items[index],
-                  style: TextStyle(
-                    color: Colors.white,
+        child: showImage
+            ? Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.clear,
+                          color: Colors.red, // Color rojo
+                        ),
+                        onPressed: clearSelectedImage,
+                      ),
+                    ],
                   ),
-                ),
-                onTap: () {
-                  print('Elemento clicado: ${items[index]}');
+                  Expanded(
+                    child: Center(
+                      child: selectedImage != null
+                          ? Image.file(
+                              File(selectedImage!.path),
+                              fit: BoxFit.contain,
+                            )
+                          : CircularProgressIndicator(),
+                    ),
+                  ),
+                ],
+              )
+            : ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 4,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    color: Colors.grey[800],
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      leading: Icon(
+                        icons[index],
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        items[index],
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onTap: () {
+                        if (index == 0) {
+                          requestPermissionsAndOpenGallery();
+                        } else {
+                          print('Elemento clicado: ${items[index]}');
+                        }
+                      },
+                    ),
+                  );
                 },
               ),
-            );
-          },
-        ),
       ),
     );
   }
